4a264e99260b2f56f09f9f7127b18b43e3f85d1c
M. Andrés Piana <mateoapiana@gmail.com>
2024-09-27 15:58:46 -0300

    adding metadata and a icon

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import 'charts.css';
 export default async function Home() {
   const registers = await getResults()
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-[#283c50]">
       <header>
         <h1 className="text-4xl text-center">Analíticas de la encuesta sobre la Inteligencia Artificial</h1>
       </header>
