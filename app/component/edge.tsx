71f15d61874ffae85437e6b25e63d0803ca53ca4
M. Andrés Piana <mateoapiana@gmail.com>
2024-09-27 15:29:06 -0300

    check the eslint bug

--- app/component/edge.tsx
+++ app/component/edge.tsx
@@ -42,6 +42,7 @@ export function Edge({ registers }: { registers: Register[] }) {
     return acc
   }, [] as data[])
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const renderCustomizedLabel = ({ cx, cy, midAngle, outerRadius, percent }: any) => {
     const RADIAN = Math.PI / 180
     const radius = outerRadius * 0.8
